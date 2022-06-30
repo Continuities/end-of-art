8ad6fcc22ccc7a819de06d4fe90a34522e850a43
Michael Townsend <michael@doublespeakgames.com>
2022-06-30 10:56:27 -0400

    - wire to art!

--- src/artist.ts
+++ src/artist.ts
@@ -5,17 +5,24 @@ if (!baseUri) {
   throw new Error('ART_URI undefined');
 }
 
-export const generate = async (text: string): Promise<string> => {
-  throw new Error('Unimplemented');
-  // const response = await fetch(baseUri, {
-  //   method: 'POST',
-  //   body: JSON.stringify({ article: text }),
-  //   headers: {
-  //     'Content-Type': 'application/json'
-  //   }
-  // });
-  // if (!response.ok) {
-  //   throw new Error(`Failed to art ${response.status}`);
-  // }
-  // return response.text();
+export const generate = async (prompt: string): Promise<HTMLImageElement> => {
+  const response = await fetch(baseUri, {
+    method: 'POST',
+    body: JSON.stringify({
+      prompt,
+      number_of_images: 4,
+      return_only_best: true
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+  if (!response.ok) {
+    throw new Error(`Failed to art ${response.status}`);
+  }
+  const { generated_images: images, generated_images_format: format } =
+    (await response.json()) as any;
+  const img = new Image();
+  img.src = `data:image/${format};base64,${images[0].image}`;
+  return img;
 };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
-import Art from './art';
+import Inspiration from './inspiration';
+import { generate } from './artist';
 
 (async () => {
-  const art = await Art();
+  const inspiration = await Inspiration();
+  console.log(`Inspired by "${inspiration}"`);
+  const art = await generate(inspiration);
   console.log(art);
 })();
